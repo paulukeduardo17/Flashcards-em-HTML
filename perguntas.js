d4a3e9cdca27542fda10c8a0d1a6abed261697b8
paulukeduardo17 <carlos.santos.pauluk@escola.pr.gov.br>
2025-10-10 08:04:12 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,13 @@
+criacartao(
+'progrramacao',
+'o que e python',
+'o python e uma linguagem de programacao'
+)
+criacartao(
+    'geografia',
+    'quala capital da franca',
+    'a capital da franca e paris'
+)
+criacartao(
+' programacao',
+
