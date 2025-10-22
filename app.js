fcf3648db8c64103a584bd85d6fd139c6dc0b4fa
paulukeduardo17 <carlos.santos.pauluk@escola.pr.gov.br>
2025-10-22 08:53:44 -0300

    ..

---
+++ app.js
@@ -0,0 +1,7 @@
+function criacartoa(categoria,perguntas,resposta){
+    let comtainer=
+    document.getElementById(container)
+    let cartao= document.document.creteelement('articl')
+    cartao.classname='cartao'
+
+}
