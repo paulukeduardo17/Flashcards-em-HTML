0e6be6cf920bf94257770532606bb7146856aad9
paulukeduardo17 <carlos.santos.pauluk@escola.pr.gov.br>
2025-11-19 08:08:08 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -1,21 +1,23 @@
-criacartao(
-'progrramacao',
-'o que e python',
-'o python e uma linguagem de programacao'
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
 )
-criacartao(
-    'geografia',
-    'quala capital da franca',
-    'a capital da franca e paris'
-)
-criacartao(
-' programacao',
-o que e uma funcao?
-'uma funcao e um bloco de codigo que executa alguma terefa
- )
- criacartao(
-    'lingua inglesa',
-    'como se diz ingles?',
-    'oi em ingles e hi(rai)'
- )
 
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
